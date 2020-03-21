e1dd4783f75aa19cb93a4b3f9203bd4131b30963
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-03-21 08:43:37 +0300

    feat: implement subtask1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,23 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var resultArr = sadArray.toMutableList()
+        var isSad: Boolean
+
+        do {
+            isSad = false
+
+            for (x in 1 until resultArr.size - 1) {
+                if (resultArr[x - 1] + resultArr[x + 1] < resultArr[x]) {
+                    resultArr.removeAt(x)
+                    isSad = true
+
+                    break
+                }
+            }
+        } while (isSad)
+
+        return resultArr.toIntArray()
     }
 }
