b52be8dd4c2db9931a155477eea87049e5de4d5a
Aliaksei_Shvants <Aliaksei_Shvants@epam.com>
2020-03-22 20:53:42 +0300

    feat: implement subtask5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,61 @@
 package subtask5
 
+private const val NINE = '9'
+private const val ERROR = "-1"
+
 class HighestPalindrome {
 
-    // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        val startPart = digitString.substring(0, n / 2).toCharArray()
+        val endPart = digitString.substring(n / 2).toCharArray()
+        val diff = mutableListOf<Int>()
+
+        endPart.reverse()
+
+        startPart.forEachIndexed { index, char ->
+            if (endPart[index] != char) diff.add(index)
+        }
+
+        if (diff.size > k) {
+            return ERROR
+        }
+
+        var x = k
+
+        loop@ while (x > 0) {
+            when {
+                diff.size > 0 -> {
+                    val index = diff[0]
+                    val element = if (startPart[index].toInt() > endPart[index].toInt()) {
+                        startPart[index]
+                    } else {
+                        endPart[index]
+                    }
+
+                    startPart[index] = element
+                    endPart[index] = element
+                    diff.remove(index)
+                    x--
+
+                    continue@loop
+                }
+                else -> {
+                    if (k > 1) {
+                        val element = startPart.firstOrNull { it != NINE }
+
+                        element?.let {
+                            startPart[startPart.indexOf(element)] = NINE
+                            endPart[endPart.indexOf(element)] = NINE
+                        }
+
+                        x -= 2
+                    }
+                }
+            }
+        }
+
+        endPart.reverse()
+
+        return "${String(startPart)}${String(endPart)}"
     }
 }
